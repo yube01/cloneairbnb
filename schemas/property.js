62f262cda6020e93866b51ca2d54e6197aa84f39
yube01 <114811769+yube01@users.noreply.github.com>
2023-02-18 20:09:54 +0545

    property js code complete

--- schemas/property.js
+++ schemas/property.js
@@ -1,11 +1,81 @@
 export default {
     name:"airbnb",
-    title:"clone",
+    title:"property",
     type:"document",
     fields:[
         {
             name:"title",
-            title:"Hey",
+            title:"Title",
+            type:"string"
+        },
+        {
+            name:"location",
+            title:"Location",
+            type:"geopoint"
+        },
+        {
+            name:"propertyType",
+            title:"Property Type",
+            type:"string",
+            options:{
+                list:[
+                    { title:"House" , value:"house"},
+                    { title:"Bed and Breakfast", value:"bed-and-breakfast" },
+                    { title:"Pool", value:"pool" },
+                    { title:"Double Bed", value:"doubleBed" }
+                ],
+                layout:"radio"
+            }
+        },
+        {
+            name:"mainImage",
+            title:"Main Image",
+            type:"image",
+            options:{
+                hotspot:true
+            }
+        },
+        {
+            name:"images",
+            title:"Images",
+            type:"array",
+            of:[
+                {type:"propertyImage"}
+            ]
+
+        },
+        {
+            name:"pricePerNight",
+            title:"Price per night",
+            type:"number"
+        },
+        {
+            name:"bed",
+            title:"Bed",
+            type:"number"
+        },
+        {
+            name:"bedrooms",
+            title:"Bedrooms",
+            type:"number"
+        },
+        {
+            name:"slug",
+            title:"Slug",
+            type:"slug",
+            options:{
+                source:"title",
+                maxLength:100
+            }
+        },
+        {
+            name:"id",
+            title:"Id",
+            type:"number"
+        },
+        {
+            name:"description",
+            title:"Description",
             type:"string"
         }
     ]
